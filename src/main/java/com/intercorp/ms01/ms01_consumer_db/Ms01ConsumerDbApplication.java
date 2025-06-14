b81f6ea3c7f185dcc0579de16bfb2a9aa19b69b7
Leonardo <56612664+LeonardoSGSG@users.noreply.github.com>
2025-06-14 02:46:04 -0500

    1st commit

---
+++ src/main/java/com/intercorp/ms01/ms01_consumer_db/Ms01ConsumerDbApplication.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms01.ms01_consumer_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms01ConsumerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms01ConsumerDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intercorp/ms01/ms01_consumer_db/Ms01ConsumerDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intercorp.ms01.ms01_consumer_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms01ConsumerDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
